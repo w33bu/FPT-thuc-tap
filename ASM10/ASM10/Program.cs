00b34145bb73a8bc00853b5a6696944f3694a084
s103582323 <117511175+w33bu@users.noreply.github.com>
2025-09-07 20:22:53 +0700

    Add files via upload

---
+++ ASM10/ASM10/Program.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+
+            log.Info("program start");
+
+            try
+            {
+                Console.Write("1st number: ");
+                int num1 = int.Parse(Console.ReadLine());
+
+                Console.Write("2nd number: ");
+                int num2 = int.Parse(Console.ReadLine());
+
+                int sum = num1 + num2;
+                log.Info($"Sum {num1} + {num2} : {sum}");
+            }
+            catch (Exception ex)
+            {
+                log.Error("error: " + ex.Message, ex);
+            }
+
+            log.Info("program closed");
+        }
+    }
+}
---
+++ ASM10/EXC2/Program.cs
@@ -0,0 +1,49 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
+
+            log.Info("program Start");
+
+            try
+            {
+                Console.Write("enter file path: ");
+                string filePath = Console.ReadLine();
+
+                if (File.Exists(filePath))
+                {
+                    log.Info($"File name: {Path.GetFileName(filePath)}");
+
+                    string[] lines = File.ReadAllLines(filePath);
+                    log.Info($"number of row: {lines.Length}");
+
+                    log.Info("content:");
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else
+                {
+                    log.Error("File doesn't exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("error: " + ex.Message, ex);
+            }
+
+            log.Info("program close");
+        }
+    }
+}
